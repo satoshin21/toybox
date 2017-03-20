c3c525e0b79489bfd9f15f1aeb4fdae07c93ce1d
Satoshi Nagasaka <satoshi.nagasaka21@gmail.com>
2017-03-20 23:35:20 +0900

    Fixed Rotation Animation

--- CoreAnimation.playground/Contents.swift
+++ CoreAnimation.playground/Contents.swift
@@ -10,15 +10,16 @@ let childView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
 view.addSubview(childView)
 childView.backgroundColor = .gray
 
-childView.layer.anchorPoint = CGPoint(x: 1, y: 0.5)
+childView.layer.anchorPoint = CGPoint(x: 1.5, y: 0.5)
 childView.center = view.center
-let animation = CABasicAnimation(keyPath: "transform.rotation.y")
+let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+
 animation.fromValue = 0
 animation.toValue = 4
 animation.duration = 2
 
-childView.layer.setValue(4, forKeyPath: "transform.rotation.y")
 childView.layer.add(animation, forKey: nil)
 
+childView.layer.setValue(4, forKeyPath: "transform.rotation.z")
 
 PlaygroundPage.current.liveView = view
